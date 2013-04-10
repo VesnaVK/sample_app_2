dd11519f4d0b0b005b5fef0625b1f8cbba18c3f3
Vesna Kovach <vkovach@madisoncollege.edu>
2013-04-09 20:48:17 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,16 @@ describe User do
       end      
     end
   end
+  
+    describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 
   describe "when email address is already taken" do
     before do
@@ -74,6 +84,8 @@ describe User do
 
     it { should_not be_valid }
   end
+  
+  
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
